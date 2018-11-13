9997deef95839838c82988ff3f356d0b44f9306b
tchivs <tchivs@163.com>
2018-11-13 15:11:57 +0800

    整理方顺序

--- ExcelLib/ExcelLib/Properties/AssemblyInfo.cs
+++ ExcelLib/ExcelLib/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
@@ -22,7 +21,7 @@ using System.Runtime.InteropServices;
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("dbee7006-1957-4e4a-b3b0-da491619cfe6")]
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
